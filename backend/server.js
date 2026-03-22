c49caf6a4517218c32ffc65554a3dc512d9c94d8
Harsh Sharma <harshusharma9896@gmail.com>
2026-03-23 01:55:13 +0530

    feature : Build Quiz APIs

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ import authRoutes from './routes/authRoutes.js';
 import documentRoutes from './routes/documentRoutes.js';
 import flashcardRoutes from './routes/flashcardRoutes.js';
 import aiRoutes from './routes/aiRoutes.js';
+import quizRoutes from './routes/quizRoutes.js';
 
 connectDB();
 
@@ -39,6 +40,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/document', documentRoutes)
 app.use('/api/flashcards', flashcardRoutes)
 app.use('/api/ai', aiRoutes)
+app.use('/api/quizzes', quizRoutes)
 
 app.use(errorHandler);
 
